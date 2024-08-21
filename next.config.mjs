43769b4ac4a5d5c04f4cbedf64e6c292ccbe6944
Blendi Maliqi <55741869+blendimaliqi@users.noreply.github.com>
2024-08-22 00:21:28 +0200

    updated next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "run-ad-auction=(), join-ad-interest-group=()",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
